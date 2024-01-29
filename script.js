5198f78e60ff68651fd6ef3fcced24a615c43514
Sebastian Orellana <orellanasebastian2002@gmail.com>
2024-01-29 12:17:21 -0300

    odin sketch donegit status

---
+++ script.js
@@ -0,0 +1,51 @@
+const main = document.querySelector(".main");
+const btn_set_value = document.querySelector(".set-value");
+const btn_clear = document.querySelector(".clear");
+
+let hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "a", "b", "c", "d", "e", "f"];
+
+
+const randomColorBackground = () => {
+	let randomColor = "#";
+
+	for(let i = 0; i <= 5; i++) {
+		randomColor += Math.floor(Math.random() * hex.length).toString(16);
+	}
+
+	return randomColor;
+}
+
+const createDivs = cuadricula => {
+
+	let cuadrado = cuadricula * cuadricula;
+
+	for(let i = 1; i <= cuadrado; i++) {
+		let div = document.createElement("div");
+		div.setAttribute("class", "divs");
+		div.style.width = main.clientWidth / cuadricula + "px";
+		div.style.height = main.clientHeight / cuadricula + "px";
+		main.appendChild(div);
+
+		div.addEventListener("mouseover", () => {
+			div.style.background = randomColorBackground();
+		})
+
+		btn_clear.addEventListener("click", () => {
+			while(main.hasChildNodes()) {
+				main.removeChild(main.firstChild);
+			}
+			btn_set_value.removeAttribute("disabled");
+		})
+	}
+
+}
+
+const clear = () => {
+
+}
+
+btn_set_value.addEventListener("click", () => {
+	let cuadricula = prompt("Indica el número de cuadrícula (max 100)");
+	createDivs(cuadricula);
+	btn_set_value.setAttribute("disabled", "");
+});
